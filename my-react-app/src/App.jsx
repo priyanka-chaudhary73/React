8008f91e69c4c04300ef1748d058d2f514d9dac7
priyanka-chaudhary73 <pinku984849@gmail.com>
2025-11-10 22:11:36 +0545

    created card

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,15 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Food from './components/Food.jsx'
+import Card from './components/Card.jsx';
 
 function App() {
  
   return(
     <>
        <Header/>
+       <Food/>
+       <Card/>
        <Footer/>
      </>
   );
---
+++ my-react-app/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className = "card">
+            <img></img>
+            <h2>Bro Code</h2>
+            <p>I am learning react and play games</p>
+        </div>
+    );
+}
+
+export default Card;
