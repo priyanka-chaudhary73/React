971a205d6c0d6cbbb3b36453d12ead680ccc5f44
priyanka-chaudhary73 <pinku984849@gmail.com>
2025-11-11 23:33:52 +0545

    Button created

--- my-react-app/src/components/Button.jsx
+++ my-react-app/src/components/Button.jsx
@@ -1,6 +1,16 @@
+
 function Button(){
+
+    const styles = {
+         backgroundColor: "hsl(200, 100%, 50%)",
+         color:" white",
+         padding: "10px, 20px",
+         borderRadius: "5px",
+         border: "none",
+         cursor: "pointer",
+    }
     return(
-        <button className = "button">Click me</button>
+        <button style = {styles}>Click me</button>
     );
 }
 
--- my-react-app/src/components/Card.jsx
+++ my-react-app/src/components/Card.jsx
@@ -2,7 +2,7 @@
 function Card(){
     return(
         <div className = "card">
-            <img className = "card-image" src ="./picture.jpg" alt = "profile picture" height={400} width={400}></img>
+            <img className = "card-image" src ="./picture.jpg" alt = "profile picture"></img>
             <h2 className = "card-title">Priyanka</h2>
             <p className = "card-text">I am learning react and play games</p>
         </div>
