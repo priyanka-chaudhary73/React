fa695860d07e185d6d5a68710e8798c1fce3237a
priyanka-chaudhary73 <pinku984849@gmail.com>
2025-11-12 22:47:15 +0545

    conditional rendering

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,7 @@ import Food from './components/Food.jsx'
 import Card from './components/Card.jsx';
 import Button from './components/Button.jsx';
 import Student from './components/Student.jsx';
+import UserGreeting from './components/UserGreeting.jsx';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
        <Student name = "Zenitsu" age = {51} isStudent = {true}/>
        <Student name = "Giyu"/>
        <Student/>
+       <UserGreeting isLoggedIn= {true} username = "Priyanka"/>
 
      </>
   );
--- my-react-app/src/components/Student.jsx
+++ my-react-app/src/components/Student.jsx
@@ -1,23 +1,19 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-function Student(props){
-    return(
-        <div className = "student">
-            <p>Name : {props.name}</p>
-            <p>Age : {props.age}</p>
-            <p>Student : {props.isStudent ? "Yes" : "No" }</p>
-        </div>
-    );
+function Student({ name = "Guest", age = 0, isStudent = false }) {
+  return (
+    <div className="student">
+      <p>Name : {name}</p>
+      <p>Age : {age}</p>
+      <p>Student : {isStudent ? "Yes" : "No"}</p>
+    </div>
+  );
 }
+
 Student.propTypes = {
-    name : PropTypes.string,
-    age : PropTypes.number,
-    isStudent : PropTypes.bool,
-}
-Student.defaultProps = {
-    name : "Guest",
-    age : 0,
-    isStudent : false,
-}
+  name: PropTypes.string,
+  age: PropTypes.number,
+  isStudent: PropTypes.bool,
+};
 
 export default Student;
